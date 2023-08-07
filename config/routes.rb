c97b9195f4af99356b20741d997ef0813cd4513d
AqsaAltaf1 <AqsaAltaf1@github.com>
2023-08-07 14:26:57 +0500

    credential added

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
+  root'welcome#index'
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+  # devise_scope :user do
+  #   root to: "devise/sessions#new"
+  # end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
