5d3501fb9d7baae940548865c395c8ceaafcd68c
AqsaAltaf1 <AqsaAltaf1@github.com>
2023-08-08 15:59:04 +0500

    edit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,8 +9,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                  587,
     domain:               'localhost:3000',
-    user_name:             Rails.application.credentials.dig(:google_smtp, :email),
-    password:              Rails.application.credentials.dig(:google_smtp, :password),
+    user_name:            'aqsaaltaf01@gmail.com',
+    password:             'akytytezdtyumxeg',
     authentication:       'plain',
     enable_starttls_auto: true
   }
