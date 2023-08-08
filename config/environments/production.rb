89a7c172b8e3e2f4351cc5a5c131d2cf5f443499
AqsaAltaf1 <AqsaAltaf1@github.com>
2023-08-08 15:15:01 +0500

    dom

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                  587,
-    domain:               'localhost',
+    domain:               'localhost:3000',
     user_name:             Rails.application.credentials.dig(:google_smtp, :email),
     password:              Rails.application.credentials.dig(:google_smtp, :password),
     authentication:       'plain',
