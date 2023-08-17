f2093946edb7fd250086fc52db7d2eda5d5ac366
AqsaAltaf1 <AqsaAltaf1@github.com>
2023-08-17 12:13:50 +0500

    room

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -23,7 +23,7 @@ class Users::SessionsController < Devise::SessionsController
   end
 
   def after_sign_in_path_for(resource_or_scope)
-    user_path(current_user) || root_path
+    root_path
   end
   # protected
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   validates :full_name, presence: true
+  scope :all_except, ->(user) { where.not(id: user) }
+  after_create_commit { broadcast_append_to "users" }
+  has_many :messages, dependent: :destroy
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable,:confirmable,
   :omniauthable, omniauth_providers: [:google_oauth2]
